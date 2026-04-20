73145150f8be04286629375c997621ddf68d56d8
Gurudatt S A <gurudatt.gd04@gmail.com>
2026-04-20 10:21:21 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig ({
   expect: {timeout: 30_000},
 
 
-  fullyParallel: true,
+  fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2: 0,
   workers: process.env.CI ? 2: 1,
